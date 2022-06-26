941b1534103d724a1b6cb457c7ed2174cbc7141a
Vilmarin <59697462+Vilmarin@users.noreply.github.com>
2022-06-26 22:55:38 +0300

    add active cat

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,13 +1,50 @@
+import { useState } from 'react'
+
 export const Categories = () => {
+    const [activeCat, setactiveCat] = useState(0)
+
+    const activCatClick = (index) => {
+        setactiveCat(index)
+    }
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                <li
+                    onClick={() => activCatClick(0)}
+                    className={activeCat === 0 ? 'active' : ' '}
+                >
+                    Все
+                </li>
+                <li
+                    onClick={() => activCatClick(1)}
+                    className={activeCat === 1 ? 'active' : ' '}
+                >
+                    Мясные
+                </li>
+                <li
+                    onClick={() => activCatClick(2)}
+                    className={activeCat === 2 ? 'active' : ' '}
+                >
+                    Вегетарианская
+                </li>
+                <li
+                    onClick={() => activCatClick(3)}
+                    className={activeCat === 3 ? 'active' : ' '}
+                >
+                    Гриль
+                </li>
+                <li
+                    onClick={() => activCatClick(4)}
+                    className={activeCat === 4 ? 'active' : ' '}
+                >
+                    Острые
+                </li>
+                <li
+                    onClick={() => activCatClick(5)}
+                    className={activeCat === 5 ? 'active' : ' '}
+                >
+                    Закрытые
+                </li>
             </ul>
         </div>
     )
