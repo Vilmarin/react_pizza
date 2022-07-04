5215098295d6fc52cc23e2b1007625247ab9f70b
Vilmarin <59697462+Vilmarin@users.noreply.github.com>
2022-07-05 00:56:12 +0300

    add mock data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 //Components
 import './App.css'
 import { Categories } from './components/Categories/Categories'
@@ -6,10 +7,21 @@ import { PizzaComponent } from './components/PizzaComponent/PizzaComponent'
 import './scss/app.scss'
 
 //Data
-import pizzas from './data/pizzas.json'
 import { Sort } from './components/Sort/Sort'
 
 function App() {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        fetch('https://62c35a07876c4700f53b18de.mockapi.io/items')
+            .then((res) => {
+                return res.json()
+            })
+            .then((arr) => {
+                setItems(arr)
+            })
+    }, [])
+
     return (
         <div className="wrapper">
             <Header />
@@ -21,7 +33,7 @@ function App() {
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((item) => (
+                        {items.map((item) => (
                             <PizzaComponent key={item.id} {...item} />
                         ))}
                     </div>
